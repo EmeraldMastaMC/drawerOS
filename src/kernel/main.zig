86b4e117b68fabc60ae36d1ba863ed56a8e5b14a
Nicholas Kasper <nick@kasperfamily.net>
2024-10-18 00:32:39 -0500

    Map a physical address to a virtual address function

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -2,7 +2,9 @@ const idt = @import("idt.zig");
 const paging = @import("paging.zig");
 const VGA_WIDTH = 80;
 const VGA_HEIGHT = 25;
-const VIDEO_MEMORY: [*]volatile u16 = @ptrFromInt(0xB8000);
+
+// const VIDEO_MEMORY: [*]volatile u16 = @ptrFromInt(0xB8000);
+const VIDEO_MEMORY: [*]volatile u16 = @ptrFromInt(0xC0000); // Proof of concept that paging works. real video memory is at 0xb8000
 
 // MODIFY THESE VALUES IF YOU MODIFY paging.TOTAL_ENTRIES
 var PML4: [*]volatile paging.PML4Entry = @ptrFromInt(0x1000);
@@ -12,6 +14,8 @@ var PT: [*]volatile paging.PTEntry = @ptrFromInt(0x4000);
 
 export fn main() noreturn {
     paging.identityMap(PML4, PDP, PD, PT);
+    paging.mapPage(@ptrFromInt(0xB8000), @ptrFromInt(0xC0000), PML4, PDP, PD, PT, false, false, false, false, false, false);
+
     idt.entry(0x0, @as(usize, @intFromPtr(&lots_of_x)));
     idt.load();
     asm volatile ("int $0x0");
