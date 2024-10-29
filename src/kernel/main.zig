f9bfc5029b8d7e3e217d66eb5618c2262e94f6e8
EmeraldMastaMC <>
2024-10-29 11:35:55 -0500

    PCI Bar functions

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -19,6 +19,10 @@ pub const PDP_ENTRIES: u64 = 1;
 pub const PD_ENTRIES: u64 = 300;
 pub const PT_ENTRIES: u64 = 512;
 
+const SERIAL_BUS_CONTROLLER: u8 = 0xC;
+const USB_CONTROLLER: u8 = 0x3;
+const XHCI_CONTROLLER: u8 = 0x30;
+
 export fn main() noreturn {
     // Use a writer that doesn't depend on interrupts to function.
     var raw_writer = console.RawWriter.new(colors.White, colors.Black);
@@ -54,44 +58,84 @@ export fn main() noreturn {
     writer.setColors(colors.White, colors.Black);
     writer.putLn();
 
-    {
-        writer.putString("Creating a 40KiB heap...\n");
-        var heap = heap_allocator.Heap.new(10);
-        writer.putString("Done.\n");
-        defer writer.putString("Done.\n");
-        defer heap.deinit();
-        defer writer.putString("Deallocating heap...\n");
-        writer.putString("Allocating 8 Byte str...\n");
-        var str: [*]u8 = @ptrCast(heap.alloc(8));
-        writer.putString("Done.\n");
-        defer writer.putString("Done.\n");
-        defer heap.free(@ptrCast(str), 8);
-        defer writer.putString("Deallocating str...\n");
-        str[0] = 'H';
-        str[1] = 'e';
-        str[2] = 'l';
-        str[3] = 'l';
-        str[4] = 'o';
-        str[5] = '!';
-        str[6] = '\n';
-        str[7] = 0x0;
-        writer.putCString(str);
-    }
+    // {
+    //     writer.putString("Creating a 40KiB heap...\n");
+    //     var heap = heap_allocator.Heap.new(10);
+    //     writer.putString("Done.\n");
+    //     defer writer.putString("Done.\n");
+    //     defer heap.deinit();
+    //     defer writer.putString("Deallocating heap...\n");
+    //     writer.putString("Allocating 8 Byte str...\n");
+    //     var str: [*]u8 = @ptrCast(heap.alloc(8));
+    //     writer.putString("Done.\n");
+    //     defer writer.putString("Done.\n");
+    //     defer heap.free(@ptrCast(str), 8);
+    //     defer writer.putString("Deallocating str...\n");
+    //     str[0] = 'H';
+    //     str[1] = 'e';
+    //     str[2] = 'l';
+    //     str[3] = 'l';
+    //     str[4] = 'o';
+    //     str[5] = '!';
+    //     str[6] = '\n';
+    //     str[7] = 0x0;
+    //     writer.putCString(str);
+    // }
 
+    var usb_slot: u8 = undefined;
+    var usb_bus: u8 = undefined;
     for (0..256) |bus| {
         for (0..256) |slot| {
-            const device = pci.checkDevice(@truncate(bus), @truncate(slot));
+            const device = pci.getDevice(@truncate(bus), @truncate(slot));
             if (device != 0xFFFF) {
-                writer.putString("Device Found: ");
-                writer.putHex(@as(u64, device));
-                writer.putLn();
-                writer.putString("    Vendor ID: ");
-                writer.putHex(@as(u64, pci.checkVendor(@truncate(bus), @truncate(slot))));
-                writer.putLn();
-                writer.flush();
+                // writer.putString("Device Found: ");
+                // writer.putHex(@as(u64, device));
+                // writer.putLn();
+                // writer.putString("    Vendor ID: ");
+                // writer.putHex(@as(u64, pci.getVendor(@truncate(bus), @truncate(slot))));
+                // writer.putLn();
+                // writer.putString("    Class ID: ");
+                // writer.putHex(@as(u64, pci.getClass(@truncate(bus), @truncate(slot))));
+                // writer.putLn();
+                // writer.putString("    Sub Class ID: ");
+                // writer.putHex(@as(u64, pci.getSubClass(@truncate(bus), @truncate(slot))));
+                // writer.putLn();
+                // writer.putString("    Prog If: ");
+                // writer.putHex(@as(u64, pci.getProgIf(@truncate(bus), @truncate(slot))));
+                // writer.putLn();
+                // writer.flush();
+                const class = pci.getClass(@truncate(bus), @truncate(slot));
+                const sub_class = pci.getSubClass(@truncate(bus), @truncate(slot));
+                const prog_if = pci.getProgIf(@truncate(bus), @truncate(slot));
+
+                if ((class == SERIAL_BUS_CONTROLLER) and (sub_class == USB_CONTROLLER) and (prog_if == XHCI_CONTROLLER)) {
+                    usb_slot = @truncate(slot);
+                    usb_bus = @truncate(bus);
+                    break;
+                }
             }
         }
     }
+    writer.putString("USB XHCI_CONTROLLER Found: ");
+    writer.putLn();
+
+    writer.putString("PCI Slot: ");
+    writer.putHex(@as(usize, usb_slot));
+    writer.putLn();
+
+    writer.putString("PCI Bus: ");
+    writer.putHex(@as(usize, usb_bus));
+    writer.putLn();
+
+    const usb_bar = pci.getBAR(usb_bus, usb_slot);
+    const usb_bar_size = pci.getBARSize(usb_bus, usb_slot, usb_bar);
+    writer.putString("PCI BAR: ");
+    writer.putHex(usb_bar);
+    writer.putLn();
+
+    writer.putString("BAR SIZE: ");
+    writer.putHex(usb_bar_size);
+    writer.putLn();
 
     writer.flush();
     while (true) {
--- src/kernel/pci.zig
+++ src/kernel/pci.zig
@@ -1,6 +1,8 @@
 const ports = @import("ports.zig");
 const CONFIG_ADDRESS: u16 = 0xCF8;
 const CONFIG_DATA: u16 = 0xCFC;
+const CONFIG_BAR0: u8 = 0x10;
+const CONFIG_BAR1: u8 = 0x14;
 
 // Header Type 0x0
 pub const Header = struct {
@@ -38,12 +40,82 @@ pub fn configReadWord(bus: u8, slot: u8, func: u8, offset: u8) u16 {
     return @truncate((ports.inl(CONFIG_DATA) >> @truncate(((offset & 2) * 8))) & 0xFFFF);
 }
 
-pub fn checkVendor(bus: u8, slot: u8) u16 {
+pub fn configReadLong(bus: u8, slot: u8, func: u8, offset: u8) u32 {
+    const lbus = @as(u32, bus);
+    const lslot = @as(u32, slot);
+    const lfunc = @as(u32, func);
+
+    // Bits 0-7: Register offset
+    // Bits 8-10: Function Number
+    // Bits 11-15: Device Number
+    // Bits 16-23: Bus Number
+    // Bits 24-30: Reserved
+    // Bit 31: Enabled bit
+    const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
+
+    ports.outl(CONFIG_ADDRESS, address);
+
+    return ports.inl(CONFIG_DATA);
+}
+pub fn configWriteLong(bus: u8, slot: u8, func: u8, offset: u8, data: u32) void {
+    const lbus = @as(u32, bus);
+    const lslot = @as(u32, slot);
+    const lfunc = @as(u32, func);
+
+    const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
+    ports.outl(CONFIG_ADDRESS, address);
+    ports.outl(CONFIG_DATA, data);
+}
+pub fn getVendor(bus: u8, slot: u8) u16 {
     const vendor = configReadWord(bus, slot, 0, 0);
     return vendor;
 }
 
-pub fn checkDevice(bus: u8, slot: u8) u16 {
+pub fn getDevice(bus: u8, slot: u8) u16 {
     const device = configReadWord(bus, slot, 0, 2);
     return device;
+}
+pub fn getClass(bus: u8, slot: u8) u8 {
+    const class = configReadWord(bus, slot, 0, 0x8 | 0x2) >> 8;
+    return @truncate(class);
+}
+
+pub fn getSubClass(bus: u8, slot: u8) u8 {
+    const sub_class = configReadWord(bus, slot, 0, 0x8 | 0x2) & 0xFF;
+    return @truncate(sub_class);
+}
+
+pub fn getProgIf(bus: u8, slot: u8) u8 {
+    const prog_if = configReadWord(bus, slot, 0, 0x8) >> 8;
+    return @truncate(prog_if);
+}
+
+pub fn getBAR0(bus: u8, slot: u8) u32 {
+    const bar0 = configReadLong(bus, slot, 0, CONFIG_BAR0);
+    return bar0;
+}
+
+pub fn getBAR1(bus: u8, slot: u8) u32 {
+    const bar1 = configReadLong(bus, slot, 0, CONFIG_BAR1);
+    return bar1;
+}
+
+pub fn getBAR(bus: u8, slot: u8) usize {
+    const bar0 = @as(u64, getBAR0(bus, slot));
+    const bar1 = @as(u64, getBAR1(bus, slot));
+    const bar = (bar1 << 32) | (bar0 & 0xFFFFFFF0);
+    return bar;
+}
+
+pub fn getBARSize(bus: u8, slot: u8, bar: u64) usize {
+    configWriteLong(bus, slot, 0, CONFIG_BAR0, 0xFFFFFFFF);
+    const low_mask = configReadLong(bus, slot, 0, CONFIG_BAR0);
+    configWriteLong(bus, slot, 0, CONFIG_BAR0, @truncate(bar & 0xFFFFFFFF));
+
+    configWriteLong(bus, slot, 0, CONFIG_BAR1, 0xFFFFFFFF);
+    const high_mask = configReadLong(bus, slot, 0, CONFIG_BAR1);
+    configWriteLong(bus, slot, 0, CONFIG_BAR1, @truncate((bar & 0xFFFFFFFF00000000) >> 32));
+
+    const size = ~((@as(u64, high_mask) << 32) | @as(u64, low_mask & 0xFFFFFFF0)) + 1;
+    return size;
 }
