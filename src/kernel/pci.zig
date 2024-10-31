781cb7fa20c387f7a917a50ee1417cf9e91d6f0a
EmeraldMastaMC <>
2024-10-31 17:32:38 -0500

    Fixed potential bug involving not clearing mem space and io space command flags

--- src/kernel/pci.zig
+++ src/kernel/pci.zig
@@ -13,6 +13,34 @@ const REGISTER1: u8 = 0x4;
 const REGISTER2: u8 = 0x8;
 const REGISTER3: u8 = 0xC;
 
+const command_register = struct {
+    const INTERRUPT_DISABLE: u16 = 0b0000_0100_0000_0000;
+    const FAST_BACK2BACK_ENABLE: u16 = 0b0000_0010_0000_0000;
+    const SERR_ENABLE: u16 = 0b0000_0001_0000_0000;
+    const PARITY_ERROR_RESPONSE: u16 = 0b0000_0000_0100_0000;
+    const VGA_PALETTE_SNOOP: u16 = 0b0000_0000_0010_0000;
+    const MEM_WRITE_AND_INVALIDATE_ENABLE: u16 = 0b0000_0000_0001_0000;
+    const SPECIAL_CYCLES: u16 = 0b0000_0000_0000_1000;
+    const BUS_MASTER: u16 = 0b0000_0000_0000_0100;
+    const MEMORY_SPACE: u16 = 0b0000_0000_0000_0010;
+    const IO_SPACE: u16 = 0b0000_0000_0000_0001;
+};
+
+const status_register = struct {
+    const DETECTED_PARITY_ERROR: u16 = 0b1000_0000_0000_0000;
+    const SIGNALED_SYSTEM_ERROR: u16 = 0b0100_0000_0000_0000;
+    const RECEIVED_MASTER_ABORT: u16 = 0b0010_0000_0000_0000;
+    const RECEIVED_TARGET_ABORT: u16 = 0b0001_0000_0000_0000;
+    const SIGNALED_TARGET_ABORT: u16 = 0b0000_1000_0000_0000;
+    const DEVSEL_TIMING_FAST: u16 = 0b0000_0000_0000_0000;
+    const DEVSEL_TIMING_MEDIUM: u16 = 0b0000_0010_0000_0000;
+    const DEVSEL_TIMING_SLOW: u16 = 0b0000_0100_0000_0000;
+    const MASTER_DATA_PARITY_ERROR: u16 = 0b0000_0001_0000_0000;
+    const FAST_BACK2BACK_CAPABLE: u16 = 0b0000_0000_1000_0000;
+    const MHZ66_CAPABLE: u16 = 0b0000_0000_0010_0000;
+    const CAPABILITIES_LIST: u16 = 0b0000_0000_0001_0000;
+    const INTERRUPT_STATUS: u16 = 0b0000_0000_0000_1000;
+};
 pub const BARType = enum(u8) {
     io_space,
     mmio32,
@@ -93,7 +121,6 @@ fn configReadWord(bus: u8, slot: u8, func: u8, offset: u8) u16 {
     const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
 
     ports.outl(CONFIG_ADDRESS, address);
-
     return @truncate((ports.inl(CONFIG_DATA) >> @truncate(((offset & 2) * 8))) & 0xFFFF);
 }
 
@@ -101,26 +128,27 @@ fn configReadLong(bus: u8, slot: u8, func: u8, offset: u8) u32 {
     const lbus = @as(u32, bus);
     const lslot = @as(u32, slot);
     const lfunc = @as(u32, func);
-
-    // Bits 0-7: Register offset
-    // Bits 8-10: Function Number
-    // Bits 11-15: Device Number
-    // Bits 16-23: Bus Number
-    // Bits 24-30: Reserved
-    // Bit 31: Enabled bit
     const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
 
     ports.outl(CONFIG_ADDRESS, address);
-
     return ports.inl(CONFIG_DATA);
 }
 
+fn configWriteWord(bus: u8, slot: u8, func: u8, offset: u8, data: u16) void {
+    const lbus = @as(u32, bus);
+    const lslot = @as(u32, slot);
+    const lfunc = @as(u32, func);
+    const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
+
+    ports.outl(CONFIG_ADDRESS, address);
+    ports.outw(CONFIG_DATA, data);
+}
 fn configWriteLong(bus: u8, slot: u8, func: u8, offset: u8, data: u32) void {
     const lbus = @as(u32, bus);
     const lslot = @as(u32, slot);
     const lfunc = @as(u32, func);
-
     const address = @as(u32, (lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | (@as(u32, 0x80000000)));
+
     ports.outl(CONFIG_ADDRESS, address);
     ports.outl(CONFIG_DATA, data);
 }
@@ -144,6 +172,10 @@ pub fn getDeviceID(bus: u8, slot: u8) u16 {
 pub fn getCommandFlags(bus: u8, slot: u8) u16 {
     const command_flags = configReadWord(bus, slot, 0, REGISTER1);
     return command_flags;
+}
+
+pub fn setCommandFlags(bus: u8, slot: u8, command_flags: u16) void {
+    configWriteWord(bus, slot, 0, REGISTER1, command_flags);
 }
 
 pub fn getStatus(bus: u8, slot: u8) u16 {
@@ -214,6 +246,12 @@ pub fn getBAR(bus: u8, slot: u8) usize {
 
 pub fn getBARSize(bus: u8, slot: u8, bar: u64) usize {
     const bar_type = getBARType(bus, slot);
+    const command_flags = getCommandFlags(bus, slot);
+    setCommandFlags(
+        bus,
+        slot,
+        command_flags & (~(command_register.MEMORY_SPACE | command_register.IO_SPACE)),
+    );
     if (bar_type == BARType.mmio64) {
         configWriteLong(bus, slot, 0, CONFIG_BAR0, 0xFFFFFFFF);
         const low_mask = configReadLong(bus, slot, 0, CONFIG_BAR0);
@@ -224,18 +262,21 @@ pub fn getBARSize(bus: u8, slot: u8, bar: u64) usize {
         configWriteLong(bus, slot, 0, CONFIG_BAR1, @truncate((bar & 0xFFFFFFFF00000000) >> 32));
 
         const size = ~((@as(u64, high_mask) << 32) | @as(u64, low_mask & 0xFFFFFFF0)) + 1;
+        setCommandFlags(bus, slot, command_flags);
         return size;
     } else if (bar_type == BARType.mmio32) {
         configWriteLong(bus, slot, 0, CONFIG_BAR0, 0xFFFFFFFF);
         const mask = configReadLong(bus, slot, 0, CONFIG_BAR0);
         configWriteLong(bus, slot, 0, CONFIG_BAR0, @truncate(bar & 0xFFFFFFFF));
 
+        setCommandFlags(bus, slot, command_flags);
         return ~(@as(u64, mask & 0xFFFFFFF0) | 0xFFFFFFFF00000000) + 1;
     } else {
         configWriteLong(bus, slot, 0, CONFIG_BAR0, 0xFFFFFFFF);
         const mask = configReadLong(bus, slot, 0, CONFIG_BAR0);
         configWriteLong(bus, slot, 0, CONFIG_BAR0, @truncate(bar & 0xFFFFFFFF));
 
+        setCommandFlags(bus, slot, command_flags);
         return ~(@as(u64, mask & 0xFFFFFFFC) | 0xFFFFFFFF00000000) + 1;
     }
 }
