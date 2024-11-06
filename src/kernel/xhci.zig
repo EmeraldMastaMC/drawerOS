67c14c4f6c5bba06e05f56663be31810f8ccc68f
EmeraldMastaMC <>
2024-11-06 04:58:32 -0600

    New xhci structs

--- src/kernel/xhci.zig
+++ src/kernel/xhci.zig
@@ -1,4 +1,7 @@
+// https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf
 const pci = @import("pci.zig");
+const page_allocator = @import("page_frame_allocator.zig");
+const paging = @import("paging.zig");
 
 const CapabilityRegisters = packed struct {
     len: u8,
@@ -60,7 +63,57 @@ const DoorbellRegisters = packed struct {
     reserved: u8,
     doorbell_task_id: u16,
 };
-pub const Controller = struct {
+
+const SlotContext = packed struct {
+    route_string: u20,
+    speed: u4,
+    reserved0: u1 = 0,
+    mtt: u1,
+    hub: u1,
+    context_entries: u5,
+    max_exit_latency: u16,
+    root_hub_port_number: u8,
+    num_of_ports: u8,
+    tt_hub_slot_id: u8,
+    tt_port_num: u8,
+    ttt: u2,
+    reserved1: u4 = 0,
+    interrupter_target: u10,
+    usb_device_address: u8,
+    reserved2: u19 = 0,
+    slot_state: u5,
+    reserved3: [2]u64 = [_]u64{0} ** 2,
+};
+
+const EndpointContext = packed struct {
+    ep_state: u3,
+    reserved0: u5 = 0,
+    mult: u2,
+    max_p_streams: u5,
+    lsa: u1,
+    interval: u8,
+    max_esit_payload_hi: u8,
+    reserved1: u1,
+    cerr: u2,
+    ep_type: u3,
+    reserved2: u1 = 0,
+    hid: u1,
+    max_burst_size: u8,
+    max_packet_size: u16,
+    ocs: u1,
+    reserved3: u3 = 0,
+    tr_dequeue_pointer_lo: u28,
+    tr_dequeue_pointer_hi: u32,
+    average_trb_len: u16,
+    max_esit_payload_lo: u16,
+    reserved4: [3]u32 = [_]u32{0} ** 3,
+};
+
+const DeviceContext = packed struct {
+    slot_context: SlotContext,
+    endpoint_contexts: [33]EndpointContext,
+};
+pub const HostController = struct {
     device: pci.Device,
     capability_registers: *CapabilityRegisters,
     operational_registers: *OperationalRegisters,
@@ -68,7 +121,6 @@ pub const Controller = struct {
     interrupter_register_sets: [*]InterrupterRegisterSet,
     runtime_registers: *RuntimeRegisters,
     doorbell_registers: [*]DoorbellRegisters,
-    max_device_slots: u8,
-    max_interrupters: u11,
-    max_ports: u8,
+
+    // TODO: Write a function do initialize the XHCI host controller
 };
