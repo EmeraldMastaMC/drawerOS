ddb8a98f2c3cd099428660879ce5ccfd7104a8f8
EmeraldMastaMC <>
2024-11-07 13:49:01 -0600

    EVEN MORE XHCI STRUCTS

--- src/kernel/xhci.zig
+++ src/kernel/xhci.zig
@@ -5,7 +5,7 @@ const paging = @import("paging.zig");
 
 const CapabilityRegisters = packed struct {
     len: u8,
-    reserved: u8,
+    reserved: u8 = 0,
     hci_version: u16,
     hcs_params1: HcsParams1,
     hcs_params2: u32,
@@ -19,21 +19,101 @@ const CapabilityRegisters = packed struct {
 const HcsParams1 = packed struct {
     max_device_slots: u8,
     max_interrupters: u11,
-    reserved: u5,
+    reserved: u5 = 0,
     max_ports: u8,
+};
+
+const HccParams1 = packed struct {
+    addressing_64_capability: bool,
+    bandwidth_negotiation_capability: bool,
+    context_size: bool,
+    port_power_control: bool,
+    port_indicators: bool,
+    light_host_controller_reset_capability: bool,
+    latency_tolerance_messaging_capability: bool,
+    no_secondary_streaming_id_support: bool,
+    parse_all_event_data: bool,
+    stopped_short_packet_capability: bool,
+    stopped_edtla_capability: bool,
+    continuous_frame_id_capability: bool,
+    maximum_primary_stream_array_size: u4,
+    xhci_extended_capabilities_pointer: u16,
+};
+
+const HccParams2 = packed struct {
+    u3_entry_capability: bool,
+    configep_command_max_exit_latency_too_large: bool,
+    force_save_context_capability: bool,
+    compliance_transition_capability: bool,
+    large_esit_payload_capability: bool,
+    configuration_information_capability: bool,
+    reserved: u26 = 0,
 };
 const OperationalRegisters = packed struct {
     usb_cmd: u32,
     usb_status: u32,
-    page_size: u32,
-    reserved0: u64,
-    device_notification_control: u32,
+    page_size: u32, // first 16 bits for page size, rest reserved, (page_size & 0xFFFF) << 12
+    reserved0: u64 = 0,
+    device_notification_control: u32, // first 16 bits for flags, rest reserved
     command_ring_control: u64,
-    reserved1: u128,
+    reserved1: u128 = 0,
     device_context_base_address_array_pointer: u64,
     configure: u32,
     reserved2: [964]u8 = [_]u8{0} ** 964,
     port_register_sets: [256]PortRegisterSet,
+};
+
+const UsbCommand = enum(u32) {
+    run_stop = 0x00000001, // RW
+    host_controller_reset = 0x00000002, // RW
+    interrupt_enable = 0x00000004, // RW
+    host_system_error_enable = 0x00000008, // RW
+    light_host_controller_reset = 0x00000080, // RW / RO
+    controller_save_state = 0x00000100, // RW
+    controller_restore_state = 0x00000200, // RW
+    enable_wrap_event = 0x00000400, // RW
+    enable_u3_mfindex_stop = 0x00000800, // RW
+    stopped_short_packet_enable = 0x00001000, // RW
+    cem_enable = 0x00002000, // RW
+};
+
+const UsbStatus = enum(u32) {
+    host_controller_halted = 0x00000001, // RO
+    host_system_error = 0x00000004, // RWC
+    event_interrupt = 0x00000008, // RWC
+    port_change_detected = 0x00000010, // RWC
+    save_state_status = 0x00000100, // RO
+    restore_state_status = 0x00000200, // RO
+    save_restore_error = 0x00000400, // RWC
+    controller_not_ready = 0x00000800, // RO
+    host_controller_error = 0x00001000, // RO
+};
+
+const DeviceNotificationControl = enum(u32) {
+    notification_enable0 = 0x00000001, // RW
+    notification_enable1 = 0x00000002, // RW
+    notification_enable2 = 0x00000004, // RW
+    notification_enable3 = 0x00000008, // RW
+    notification_enable4 = 0x00000010, // RW
+    notification_enable5 = 0x00000020, // RW
+    notification_enable6 = 0x00000040, // RW
+    notification_enable7 = 0x00000080, // RW
+    notification_enable8 = 0x00000100, // RW
+    notification_enable9 = 0x00000200, // RW
+    notification_enable10 = 0x00000400, // RW
+    notification_enable11 = 0x00000800, // RW
+    notification_enable12 = 0x00001000, // RW
+    notification_enable13 = 0x00002000, // RW
+    notification_enable14 = 0x00004000, // RW
+    notification_enable15 = 0x00008000, // RW
+};
+
+const CommandRingControl = enum(u64) {
+    ring_cycle_state = 0x0000000000000001, // RW
+    command_stop = 0x0000000000000002, // RWC
+    command_abort = 0x0000000000000004, // RWC
+    command_ring_running = 0x0000000000000008, // RO
+    command_ring_pointer = 0xFFFFFFFFFFFFFFC0, // RW
 };
 
 const PortRegisterSet = packed struct {
@@ -45,7 +125,7 @@ const PortRegisterSet = packed struct {
 
 const RuntimeRegisters = packed struct {
     microframe_index: u32,
-    reserved: u224,
+    reserved: u224 = 0,
     interrupter_register_sets: [1024]InterrupterRegisterSet,
 };
 
@@ -53,14 +133,14 @@ const InterrupterRegisterSet = packed struct {
     interrupter_management: u32,
     interrupter_moderation: u32,
     event_ring_segment_table_size: u32,
-    reserved: u32,
+    reserved: u32 = 0,
     event_ring_segment_table_base_address: u64,
     event_ring_dequeue_pointer: u64,
 };
 
 const DoorbellRegisters = packed struct {
     doorbell_target: u8,
-    reserved: u8,
+    reserved: u8 = 0,
     doorbell_task_id: u16,
 };
 
@@ -68,8 +148,8 @@ const SlotContext = packed struct {
     route_string: u20,
     speed: u4,
     reserved0: u1 = 0,
-    mtt: u1,
-    hub: u1,
+    mtt: bool,
+    hub: bool,
     context_entries: u5,
     max_exit_latency: u16,
     root_hub_port_number: u8,
@@ -90,17 +170,17 @@ const EndpointContext = packed struct {
     reserved0: u5 = 0,
     mult: u2,
     max_p_streams: u5,
-    lsa: u1,
+    lsa: bool,
     interval: u8,
     max_esit_payload_hi: u8,
-    reserved1: u1,
+    reserved1: bool,
     cerr: u2,
     ep_type: u3,
     reserved2: u1 = 0,
-    hid: u1,
+    hid: bool,
     max_burst_size: u8,
     max_packet_size: u16,
-    ocs: u1,
+    ocs: bool,
     reserved3: u3 = 0,
     tr_dequeue_pointer_lo: u28,
     tr_dequeue_pointer_hi: u32,
